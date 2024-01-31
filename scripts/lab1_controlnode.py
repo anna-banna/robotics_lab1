b5d4b0bec7bbff12e370b446ded7d54c458566a9
anna-banna <aburgess1098@gmail.com>
2024-01-31 21:47:06 +0000

    changed pub/sub

--- scripts/lab1_controlnode.py
+++ scripts/lab1_controlnode.py
@@ -3,14 +3,21 @@
 #2. 
 
 import rospy
-from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
 from robotics_lab1.msg import Turtlecontrol
 import math 
 
-turtle_control_msg = Turtlecontrol()
-
+ROTATION_SCALE = 180.0/math.pi 
+pos_msg = Pose()
 def pose_callback(data):
+	global pos_msg
+	pos_msg.x = data.x
+	pos_msg.y = data.y
+	pos_msg.theta = data.theta * ROTATION_SCALE
+
+
+turtle_control_msg = Turtlecontrol()
+def control_callback(data):
 	global turtle_control_msg
 	turtle_control_msg.kp =
 	turtle_control_msg.xd = 
@@ -19,7 +26,8 @@ def pose_callback(data):
 if __name__ == '__main__': 
 	rospy.init_node('robotics_lab1', anonymous = True)
 	rospy.Subscriber('/turtle1/pose', Pose, pose_callback)
-	cmd_pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size = 10)
+	pose_pub = rospy.Publisher('/turtle1/control_params', Turtlecontrol, queue_size = 10)
+	rospy.Subscriber('turtle1/control_params;', Turtlecontrol, control_callback)
 	loop_rate = rospy.Rate(10)
 	vel_cmd = Twist()
 	while not rospy.is_shutdown():
